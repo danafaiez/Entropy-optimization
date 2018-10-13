4c0cc71af95396dd745b084746142ce2468eacdc
Obs. Entropy <dfaiez@ucsc.edu>
2018-10-13 10:36:25 -0700

    update

--- errorbar_minSxeSent_L_CG_B0.01.py
+++ errorbar_minSxeSent_L_CG_B0.01.py
@@ -37,13 +37,13 @@ Sent[3] = 'minSent_L16_bath12_CG_B0.01.d'
 Sent[4] = 'minSent_L20_bath16_CG_B0.01.d'
 
 Sent_ave = [1.4821405,1.4680688,1.32090275,1.19370775]
-
+"""
 S_th_loc4=1.002194
 #corresponding Beta:0.5 (compared to B=0.01)
 S_th_tot=[1.251,1.618,2.368,2.799]
 #corresponding Beta=[0.54,0.568,0.506,0.501]
 R_S_th=np.divide(S_th_loc4,S_th_tot)
-"""
+
 Sent_ave[8]   = 1.4821405
 Sent_ave[L12] = 1.4680688
 Sent_ave[L16] = 1.32090275
@@ -80,7 +80,7 @@ for i in range(1,5):
        R_min= np.divide(min(a),Sxe_ave[i-1])
        R_max= np.divide(max(a),Sxe_ave[i-1])
       
-       plt.errorbar(N[i-1],R_mean, yerr=np.array([[R_mean-R_min ,R_max-R_mean]]).T,fmt='g*',elinewidth=0.6,
+       plt.errorbar(L[i-1],R_mean, yerr=np.array([[R_mean-R_min ,R_max-R_mean]]).T,fmt='g*',elinewidth=0.6,
                ms=3,capsize=2,label=r'$R(S_{xE})$,$\Delta{x}=4$'if i == 1 else "")
 
 
@@ -90,10 +90,9 @@ for i in range(1,5):
        R_min= np.divide(min(a),Sent_ave[i-1])
        R_max= np.divide(max(a),Sent_ave[i-1])
 
-       plt.errorbar(N[i-1],R_mean, yerr=np.array([[R_mean-R_min ,R_max-R_mean]]).T,fmt='m*',elinewidth=0.6,
+       plt.errorbar(L[i-1],R_mean, yerr=np.array([[R_mean-R_min ,R_max-R_mean]]).T,fmt='m*',elinewidth=0.6,
                 ms=3,capsize=2,label=r'$R(S_{ent})$,Bath$=L-4$'if i == 1 else "")
-
-
+"""
 for i in range(1,5):
        a = loadtxt(Sent_half[i])
        R_mean= np.divide(mean(a),Sent_half_ave[i-1])
@@ -102,24 +101,15 @@ for i in range(1,5):
 
        plt.errorbar(N[i-1],R_mean, yerr=np.array([[R_mean-R_min,R_max-R_mean]]).T,fmt='b*',elinewidth=0.6,
                 ms=3,capsize=2,label=r'$R(S_{ent})$,Bath$=L/2$'if i == 1 else "")
-
-plt.scatter(N,R_S_th)
-
-#Ticks
-ax.set_axisbelow(True)
-ax.minorticks_on()
-ax.grid(which='major', linestyle=':', linewidth='0.5', color='gray')
-ax.grid(which='minor', linestyle=':', linewidth='0.5', color='black')
-ax.tick_params(which='both',top='off',left='on',right='on', bottom='on') 
-
-
+"""
+#plt.scatter(N,R_S_th)
 # Adding plotting parameters
 plt.legend()
 #plt.title('Sxe is minimized with sob=4, cmplx Gaussian coeff, 2 particles, B=0.01')
-ax.set_xlabel('N', fontsize=12)
+ax.set_xlabel('L', fontsize=12)
 ax.set_ylabel('R', fontsize=12)
 #ax.set_yticks(np.arange(0.5,1.05,0.1))
 ax.set_yticks(np.arange(0,1.1,0.1))
-ax.set_xticks(N)
+ax.set_xticks(L)
 plt.savefig("min_Sent_Sxe",bbox_inches='tight')
 show()
